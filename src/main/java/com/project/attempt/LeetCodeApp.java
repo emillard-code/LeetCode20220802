56ba99a9fc2a58d3e755afd3c9a1655ab50b67fd
Emerson Millard <emillard.code@gmail.com>
2022-08-08 12:00:00 -0700

    Added attempt class.

---
+++ src/main/java/com/project/attempt/LeetCodeApp.java
@@ -0,0 +1,39 @@
+package com.project.attempt;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class LeetCodeApp {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = new int[][] {
+            {1,5,9},
+            {10,11,13},
+            {12,13,15}
+        };
+
+        System.out.println(method(matrix, 8));
+
+    }
+
+    public static int method(int[][] matrix, int target) {
+
+        ArrayList<Integer> newMatrix = new ArrayList<Integer>();
+
+        for (int numberArr[] : matrix) {
+            for (int number : numberArr) {
+                newMatrix.add(number);
+            }
+        }
+
+        Collections.sort(newMatrix);
+
+        return newMatrix.get(target - 1);
+
+    }
+
+}
